32241eff447be724838381e113138411386ea503
Jon Zimbel <jzimbel@gmail.com>
2017-04-20 23:42:44 -0400

    Make things slightly less bad; prepare to build cluster playlists

--- app.py
+++ app.py
@@ -18,10 +18,77 @@ feature_names = [
 features_json = 'features.json'
 
 
+class TrackClusterer(object):
+    def __init__(self):
+        token = util.prompt_for_user_token(Config.USER_ID, Config.SPOTIFY_ACCESS_SCOPE)
+        if token:
+            self.sp = spotipy.Spotify(auth=token)
+            self.sp.trace = False
+        else:
+            print "Can't get token for", Config.USER_ID
+            sys.exit(1)
+
+    def get_library_features(self):
+        '''
+        Pull track audio features from Spotify.
+        Store them in features.json and return them in a dict.
+        '''
+        feature_dict = self.build_feature_dict()
+        with open(features_json, 'w+') as f:
+            serializable_feature_dict = [(k, v) for k,v in feature_dict.iteritems()]
+            json.dump(serializable_feature_dict, f, indent=2)
+        print 'Ordered feature dict stored in {}.'.format(features_json)
+        return feature_dict
+
+    def build_feature_dict(self):
+        '''
+        Returns a dict of track_id: (acousticness, danceability, energy, instrumentalness,
+        liveness, speechiness, valence) k/v pairs for all (well, most) tracks in the user's library.
+        Some tracks don't have audio features for some reason.
+        '''
+        # get ids of all tracks in the user's library
+        print 'Getting your tracks\' audio features from Spotify...'
+        saved_track_ids = []
+        results = self.sp.current_user_saved_tracks(limit=50)
+        while True:
+            items = results['items']
+            saved_track_ids.append([item['track']['id'] for item in items])
+            if results['next'] == None:
+                break
+            # let us never speak of this
+            next_offset = int(filter(lambda x: x.startswith('offset'), results['next'].split('?')[1].split('&'))[0].split('=')[1])
+            results = self.sp.current_user_saved_tracks(limit=50, offset=next_offset)
+
+        # get audio features for each track. Throw out tracks that don't have features.
+        feature_dict = OrderedDict()
+        for track_id_chunk in saved_track_ids:
+            audio_features_response = self.sp.audio_features(tracks=track_id_chunk)
+            # only keep values we care about. Ignore feature sets that have null values.
+            feature_dict_chunk = {
+                item['id']: tuple(item[name] * 100 for name in feature_names)
+                for item in audio_features_response
+                if not any(value is None for value in item.values())
+            }
+            feature_dict.update(feature_dict_chunk)
+        print 'Done! Found {} tracks with audio features in your library.'.format(len(feature_dict.keys()))
+        return feature_dict
+
+    def build_cluster_playlists(self):
+        pass
+
+
 def main(feature_dict):
     '''
     Do clustering things.
     '''
+    Config.USER_ID = args.id
+    track_clusterer = TrackClusterer()
+    if args.file is None:
+        feature_dict = track_clusterer.get_library_features()
+    else:
+        with open(args.file) as f:
+            feature_dict = OrderedDict([(k, tuple(v)) for k,v in json.load(f)])
+
     feature_array = numpy.array(feature_dict.values())
     n_clusters = 8
     result = KMeans(n_clusters=n_clusters, init='k-means++').fit(feature_array)
@@ -32,81 +99,15 @@ def main(feature_dict):
     # build playlists for each cluster
 
 
-def get_library_features():
-    '''
-    Pull track audio features from Spotify.
-    Store them in features.json and return them in a dict.
-    '''
-    token = util.prompt_for_user_token(Config.USER_ID, Config.SPOTIFY_ACCESS_SCOPE)
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        sp.trace = False
-        feature_dict = build_feature_dict(sp)
-        with open(features_json, 'w+') as f:
-            serializable_feature_dict = [(k, v) for k,v in feature_dict.iteritems()]
-            json.dump(serializable_feature_dict, f, indent=2)
-        print 'Ordered feature dict stored in {}.'.format(features_json)
-        return feature_dict
-    else:
-        print "Can't get token for", Config.USER_ID
-
-
-def build_feature_dict(sp):
-    '''
-    Returns a dict of track_id: (acousticness, danceability, energy, instrumentalness,
-    liveness, speechiness, valence) k/v pairs for all (well, most) tracks in the user's library.
-    Some tracks don't have audio features for some reason.
-    '''
-    # get ids of all tracks in the user's library
-    print 'Getting your tracks\' audio features from Spotipy...'
-    saved_track_ids = []
-    results = sp.current_user_saved_tracks(limit=50)
-    while True:
-        items = results['items']
-        saved_track_ids.append([item['track']['id'] for item in items])
-        if results['next'] == None:
-            break
-        # let us never speak of this
-        next_offset = int(filter(lambda x: x.startswith('offset'), results['next'].split('?')[1].split('&'))[0].split('=')[1])
-        results = sp.current_user_saved_tracks(limit=50, offset=next_offset)
-
-    # get audio features for each track. Throw out tracks that don't have features.
-    feature_dict = OrderedDict()
-    for track_id_chunk in saved_track_ids:
-        audio_features_response = sp.audio_features(tracks=track_id_chunk)
-        # only keep values we care about. Ignore feature sets that have null values.
-        feature_dict_chunk = {
-            item['id']: tuple(item[name] * 100 for name in feature_names)
-            for item in audio_features_response
-            if not any(value is None for value in item.values())
-        }
-        feature_dict.update(feature_dict_chunk)
-    print 'Done! Found {} tracks with audio features in your library.'.format(len(feature_dict.keys()))
-    return feature_dict
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Cluster tracks in your Spotify library by their audio features.' +\
                     '\nThe first time you run this, your library\'s audio features are cached' +\
-                    '\nin {}. After that, you can run without talking to Spotify using `python {} {}`'.format(
+                    '\nin {}. After that, you can run with fewer Spotify API requests using `python {} {}`'.format(
                         features_json, sys.argv[0], '-f {}'.format(features_json)
                     )
     )
-    parser.add_argument('-i', '--id', help='Your Spotify ID. Instructions for finding your ID: https://community.spotify.com/t5/Accounts/how-do-i-find-my-spotify-user-id/m-p/665688#M86799')
+    parser.add_argument('id', help='Your Spotify ID. Instructions for finding your ID: https://community.spotify.com/t5/Accounts/how-do-i-find-my-spotify-user-id/m-p/665688#M86799')
     parser.add_argument('-f', '--file', help='JSON file to load features from instead of pulling from Spotify')
     args = parser.parse_args()
-
-    if args.id is None and args.file is None:
-        print 'Error: Either a Spotify ID or a JSON features file must be provided.'
-        print 'See help with `python {} -h`'.format(sys.argv[0])
-        sys.exit(1)
-
-    if args.file is None:
-        Config.USER_ID = args.id
-        main(get_library_features())
-    else:
-        feature_dict = None
-        with open(args.file) as f:
-            feature_dict = OrderedDict([(k, tuple(v)) for k,v in json.load(f)])
-        main(feature_dict)
+    main(args)
--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     SPOTIPY_REDIRECT_URI = os.environ['SPOTIPY_REDIRECT_URI'] = 'https://www.google.com'
     SPOTIPY_CLIENT_ID = os.environ['SPOTIPY_CLIENT_ID'] = '76159224cd0e4725826934d253118290'
     SPOTIPY_CLIENT_SECRET = os.environ['SPOTIPY_CLIENT_SECRET'] = 'fd3f120077a3491898bb444c5a23beef'
-    SPOTIFY_ACCESS_SCOPE = 'user-library-read playlist-read-private playlist-read-public playlist-modify-private'
+    SPOTIFY_ACCESS_SCOPE = 'user-library-read playlist-read-private playlist-modify-private'
